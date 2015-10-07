55071b2ff37f5280a3bcf89c2f30f4f344f619b6
Todd Metheny <todd.metheny@gmail.com>
2015-10-06 23:24:42 -0400

    added Main.js file and some styling

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,33 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main')
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
-
-class githubNotetaker extends React.Component{
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-};
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111',
   },
 });
+
+class githubNotetaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
+    );
+  }
+};
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
